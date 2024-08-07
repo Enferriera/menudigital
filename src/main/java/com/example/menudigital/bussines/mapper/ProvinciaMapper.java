2d7760316df98769493a914ecacf70c14e7d9789
Ariel Enferrel <enferrelariel@hotmail.com>
2024-08-07 18:31:59 -0300

    agregado provincia y pais en el domicilioDto

--- src/main/java/com/example/menudigital/bussines/mapper/ProvinciaMapper.java
+++ src/main/java/com/example/menudigital/bussines/mapper/ProvinciaMapper.java
@@ -5,7 +5,7 @@ import com.example.menudigital.domain.dtos.provinciaDto.ProvinciaDto;
 import com.example.menudigital.domain.entities.Provincia;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {PaisMapper.class})
 public interface ProvinciaMapper extends BaseMapper<Provincia, ProvinciaDto>{
 
 }
